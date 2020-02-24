2e51f9f8646f8b8e2a4bd4aa30f2ad12cb88c267
CrowFea <wendaoliu40@outlook.com>
2020-02-24 22:16:10 +0800

    新增场景

---
+++ POBA/POBA/Assets/POBA/jump.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class jump: MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("LoadSceneA");
+    }
+
+    public void LoadA(string scenename)
+    {
+        Debug.Log("sceneName to load: " + scenename);
+        SceneManager.LoadScene(scenename);
+    }
+}
